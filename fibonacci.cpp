ab5f485d82025a594948b84a57e9c0159b98dbae
gupta kunal <132100570+testgithubgupta@users.noreply.github.com>
2023-05-21 15:23:21 +0530

    Create fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,31 @@
+enter the term = 5
+  01123
+  
+  
+  
+  
+  
+  
+  
+#include <iostream>
+
+
+using namespace std;
+int main()
+{  int i,n,a=0,b=1,c=0;
+
+
+    cout<< "enter the term";
+    cin>> n;
+    for(i=1;i<=n;i++)
+       { cout << c;
+        a=b;
+        b=c;
+        c=a+b;
+       }
+        
+
+    
+
+    return 0;
+}

94a178b5239d76bb0eed362bd97fb235538dcdbd
gupta kunal <132100570+testgithubgupta@users.noreply.github.com>
2023-05-21 15:47:48 +0530

    Create reverse of a number.cpp

---
+++ reverse of a number.cpp
@@ -0,0 +1,66 @@
+enter the number = 123
+    reverse of a number is 321
+#include <iostream>
+
+
+using namespace std;
+int main()
+{  int i,n,r,y=0;
+
+
+    cout<< "enter the number";
+    cin>> n;
+    while(n!=0)
+    {
+        r=n%10;
+        y=y*10+r;
+        n=n/10;
+    }
+    cout<< " reverse of number is" << y;
+    
+
+       
+        
+
+    
+
+    return 0;
+}3
+  reverse of a number is 321
+  
+  
+  
+  /******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+
+using namespace std;
+int main()
+{  int i,n,r,y=0;
+
+
+    cout<< "enter the number";
+    cin>> n;
+    while(n!=0)
+    {
+        r=n%10;
+        y=y*10+r;
+        n=n/10;
+    }
+    cout<< " reverse of number is" << y;
+    
+
+       
+        
+
+    
+
+    return 0;
+}
